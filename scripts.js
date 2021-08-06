84f280a42b44a189b63ef2506c87ec2af63825af
Richard W <riwi2802@colorado.edu>
2021-08-05 20:09:13 -0400

    step 2 round 3

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener('load', function () {
+    const flightStatus = document.getElementById('flightStatus');
+    const takeOffButton = document.getElementById('takeoff');
+    const shuttleBackground = document.getElementById('shuttleBackground')
+    const shuttleHeight = document.getElementById('spaceShuttleHeight')
+
+
+
+    takeOffButton.addEventListener('click', function() {
+        const result = window.confirm('Confirm that the shuttle is ready for take off.'); 
+        if (result) {
+            flightStatus.innerHTML = 'Shuttle in flight!';
+            shuttleBackground.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = "10,000"
+
+        }
+    })
+});
